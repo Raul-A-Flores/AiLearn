0c0e07972dc2ee2193225c4759ca8672333018b1
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-11-14 09:47:35 -0800

    gpt and unsplash api implemented

---
+++ src/lib/youtube.ts
@@ -0,0 +1,20 @@
+export async function searchYouTube(searchQuery: string) {
+    searchQuery = searchQuery.replaceAll(" ", "+");
+    console.count("youtube search");
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API}&q=${searchQuery}&videoDuration=medium&videoEmbeddable=true&type=video&maxResults=5`,
+      {
+        method: "GET",
+      }
+    );
+    const json = await response.json();
+    if (!json) {
+      console.log("youtube fail");
+      return null;
+    }
+    if (json.items[0] == undefined) {
+      console.log("youtube fail");
+      return null;
+    }
+    return json.items[0].id.videoId;
+  }
