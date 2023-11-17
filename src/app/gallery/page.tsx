524b2a8eff5c79a03fa3bcf26859b3e7f6cb75d6
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-11-17 09:39:12 -0800

    gallery added

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import GalleryCourseCard from "@/components/GalleryCourseCard";
+import { prisma } from "@/lib/db";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-const Gallery = (props: Props) => {
+const GalleryPage = async (props: Props) => {
+  const courses = await prisma.course.findMany({
+    include: {
+      units: {
+        include: { chapters: true },
+      },
+    },
+  });
   return (
-    <div>Gallery</div>
-  )
-}
+    <div className="py-8 mx-auto max-w-7xl">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center">
+        {courses.map((course) => {
+          return <GalleryCourseCard course={course} key={course.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Gallery
+export default GalleryPage;
